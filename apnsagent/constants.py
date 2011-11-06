9dde6f7f434c4a9410f3b350548ff717121b2588
jeffkit <jeff@fallever.com>
2011-11-06 16:48:48 +0800

    提供运行server的便利脚本

---
+++ apnsagent/constants.py
@@ -0,0 +1,12 @@
+#encoding=utf-8
+
+DEVELOP_DIR = 'develop'
+PRODUCTION_DIR = 'production'
+CER_FILE = 'cer.pem'
+KEY_FILE = 'key.pem'
+
+PUSH_JOB_CHANNEL = 'push_job'
+PUSH_JOB_FALLBACK = 'push_job_fallback'
+PUSH_JOB_CHANNEL_DEV = 'push_job_dev'
+PUSH_JOB_FALLBACK_DEV = 'push_job_fallback_dev'
+DEBUG_TOKENS = 'debug_tokens'
